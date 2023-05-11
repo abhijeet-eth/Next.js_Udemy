fe44bc90a0b7f5e175d3d162a67f671590d40ec4
Sinha <abhijeet.d.sinha@capgemini.com>
2023-05-11 13:10:38 +0530

    Till section 3

--- pages/clients/[id]/index.js
+++ pages/clients/[id]/index.js
@@ -3,9 +3,15 @@ import { useRouter } from "next/router";
 function ClientProjectPage() {
   const router = useRouter();
   console.log(router.query);
+
+  function loadProjectHandler() {
+    router.push("/clients/max/projecta");
+  }
+
   return (
     <>
       <h1>The project of a given Client</h1>
+      <button onClick={loadProjectHandler}>Load Project A</button>
     </>
   );
 }
